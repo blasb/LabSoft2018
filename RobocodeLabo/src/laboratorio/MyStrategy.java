03a565766b014e5c0f28fdf4827fd3b485651c16
pedroodb <dalbianco.pedro@gmail.com>
2019-09-02 19:14:05 -0300

    Prueba

--- RobocodeLabo/src/laboratorio/MyStrategy.java
+++ RobocodeLabo/src/laboratorio/MyStrategy.java
@@ -4,27 +4,21 @@ public class MyStrategy extends Strategy {
 
 	public MyStrategy(LaboRobot robot) {
 		super(robot);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public void run() {
-		robot.setColors(000000, 000000, 000000, 000000, 000000);
-		if(robot.robotX > 100) robot.turnTo(270); else robot.turnTo(90); 
+		robot.setColors(111111, 111111, 111111, 111111, 111111);
+		robot.turnTo(270); 
 		while(robot.robotX > 20) {
 			robot.ahead(robot.robotX - 20);
 		}
-		if(robot.robotY < 100) robot.turnTo(0);
-		int lado = 0;
+		if(robot.robotY < 100) robot.turnTo(180) else robot.turnTo(0);
 		while(true) {
-			if(robot.robotY > 500) robot.turnTo(90);
+			if(robot.robotY > 500) robot.turnTo(0);
+			if(robot.robotY < 100) robot.turnTo(180);
 			robot.ahead(100);
-			robot.ahead(100);		}
-	}
-	
-	private void recorrerLado(int direccion, int largo, char eje) {
-		if(eje == 'x') {
-			
-		}
+			robot.ahead(100);
+		}*
 	}
 	
 	public void onScannedRobot() {
