0211f7437b48c8f879e9bbe3af4ebe198404244b
Blas <blasbutera69@gmail.com>
2019-09-02 21:37:42 -0300

    Funciona patrulla

--- RobocodeLabo/src/laboratorio/MyStrategy.java
+++ RobocodeLabo/src/laboratorio/MyStrategy.java
@@ -1,28 +1,30 @@
 package laboratorio;
 
 public class MyStrategy extends Strategy {
-
+	
 	public MyStrategy(LaboRobot robot) {
 		super(robot);
 	}
 	
 	public void run() {
-		robot.setColors(111111, 111111, 111111, 111111, 111111);
-		robot.turnTo(270); 
+		robot.setColors(LaboRobot.blue, LaboRobot.yellow, LaboRobot.blue, LaboRobot.yellow, LaboRobot.blue);
+		//Ir a la pared izquierda
+		robot.turnTo(270);
 		while(robot.robotX > 20) {
 			robot.ahead(robot.robotX - 20);
 		}
-		if(robot.robotY < 100) robot.turnTo(180) else robot.turnTo(0);
+		
+		//Patrullar
+		robot.turnTo(0);
 		while(true) {
-			if(robot.robotY > 500) robot.turnTo(0);
-			if(robot.robotY < 100) robot.turnTo(180);
+			if(robot.robotY < 100) robot.turnTo(0);
+			if(robot.robotY > 500) robot.turnTo(180);
 			robot.ahead(100);
-			robot.ahead(100);
-		}*
+		}
 	}
-	
+
 	public void onScannedRobot() {
-		robot.fire(1);
+			robot.fire(1);
 	}
 	
 	public void onHitByBullet() {
