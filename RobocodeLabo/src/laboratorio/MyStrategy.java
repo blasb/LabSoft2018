e7c1eed87a5a101d6b03a25f580746a0838c2cc4
Blas <blasbutera69@gmail.com>
2019-09-02 22:35:52 -0300

    funcionando. no detecta enemigos

--- RobocodeLabo/src/laboratorio/MyStrategy.java
+++ RobocodeLabo/src/laboratorio/MyStrategy.java
@@ -1,13 +1,18 @@
 package laboratorio;
 
+import robocode.ScannedRobotEvent;
+
 public class MyStrategy extends Strategy {
+	
+
+	private boolean patrullando = false;
 	
 	public MyStrategy(LaboRobot robot) {
 		super(robot);
 	}
 	
 	public void run() {
-		robot.setColors(LaboRobot.blue, LaboRobot.yellow, LaboRobot.blue, LaboRobot.yellow, LaboRobot.blue);
+		robot.setColors(0, 0, 0, 0, 0);
 		//Ir a la pared izquierda
 		robot.turnTo(270);
 		while(robot.robotX > 20) {
@@ -17,18 +22,39 @@ public class MyStrategy extends Strategy {
 		//Patrullar
 		robot.turnTo(0);
 		while(true) {
-			if(robot.robotY < 100) robot.turnTo(0);
-			if(robot.robotY > 500) robot.turnTo(180);
+			patrullando = true;
+			if(robot.robotY < 100) {
+				//BOOOKEE
+				robot.setColors(LaboRobot.blue, LaboRobot.yellow, LaboRobot.blue, LaboRobot.yellow, LaboRobot.blue);
+				robot.turnTo(0);
+			}
+			//En dos partes para que no gire apuntando a la pared.
+			if(robot.robotY > 500) {
+				robot.setColors(LaboRobot.yellow, LaboRobot.blue, LaboRobot.yellow, LaboRobot.blue, LaboRobot.yellow);
+				robot.turnTo(90);
+				robot.turnTo(180);
+			}
 			robot.ahead(100);
 		}
 	}
 
 	public void onScannedRobot() {
-			robot.fire(1);
+		if(patrullando) {			
+			if (robot.scannedDistance < 100){
+				robot.fire(3);
+			}
+			else {
+				robot.fire(1);
+			}
+		}
 	}
 	
 	public void onHitByBullet() {
-		
+		if(robot.hitByBulletAngle == 0) {
+			robot.turnGunTo(0);
+		}else if(robot.hitByBulletAngle == 180) {
+			robot.turnGunTo(180);
+		}
 	}
 	
 	public void onHitWall(){
